7a2a6ec451dd8572a76529310c05f1e838b4f974
cosmo-zero <110287972+cosmo-zero@users.noreply.github.com>
2023-04-19 17:12:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.before_request
+def before_request():
+    request.remote_addr = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+@app.route("/")
 def index():
     ip_address = request.remote_addr
-    return f'Your IP address is: {ip_address}'
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return f"Your IP address is: {ip_address}"
